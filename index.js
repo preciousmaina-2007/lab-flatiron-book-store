37707969ed7f82c38b9aaa8024f568aaaf0c17a8
precious <precious.maina@student.moringaschool.com>
2026-04-16 08:56:30 +0300

    complited DOM manipulation lab

--- index.js
+++ index.js
@@ -44,4 +44,28 @@ const bookStore = {
 }
 
 // Write your code here!
+const bookStoreTitle = document.getElementById("header");
+const bookList = document.getElementById("book-list");
 
+// update title
+bookStoreTitle.textContent = bookStore.name;
+
+// loop through books
+bookStore.books.forEach(book => {
+
+    const bookContainer = document.createElement("li");
+    const bookTitle = document.createElement("h3");
+    const bookAuthor = document.createElement("p");
+    const bookImage = document.createElement("img");
+
+    bookTitle.textContent = book.title;
+    bookAuthor.textContent = book.author;
+    bookImage.src = book.imageUrl;
+    bookImage.alt = book.title;
+
+    bookContainer.appendChild(bookTitle);
+    bookContainer.appendChild(bookAuthor);
+    bookContainer.appendChild(bookImage);
+
+    bookList.appendChild(bookContainer);
+});
